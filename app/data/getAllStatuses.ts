bbca872fdc12f709ce25a880190994523e5b6b48
icedjello <revues.counter0x@icloud.com>
2024-05-02 16:45:22 +0100

    add sorting to statuses

--- app/data/getAllStatuses.ts
+++ app/data/getAllStatuses.ts
@@ -68,17 +68,33 @@ export enum Name {
 export async function getAllStatuses() {
   const fetchModeStatus = (mode: string) =>
     fetch(`https://api.tfl.gov.uk/line/mode/${mode}/status`, {
-      headers: {
-        "Content-Type": "application/xml",
-      },
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
     });
-  // "Content-type": "application/json",
 
   const allResponses = await Promise.all([
     fetchModeStatus("overground"),
     fetchModeStatus("tube"),
     fetchModeStatus("elizabeth-line"),
   ]);
-  const data = await Promise.all(allResponses.map((r) => r.json()));
-  return data.flat() as LineAndStatus[];
+
+  // const contentType = response.headers.get("content-type");
+  // if (!contentType || !contentType.includes("application/json")) {
+  //   throw new TypeError("Oops, we haven't got JSON!");
+  // }
+
+  const data = (
+    await Promise.all(allResponses.map((r) => r.json()))
+  ).flat() as LineAndStatus[];
+
+  return data.sort((a, b) => {
+    const aMinSeverity = Math.min(
+      ...a.lineStatuses.map(({ statusSeverity }) => statusSeverity)
+    );
+    const bMinSeverity = Math.min(
+      ...b.lineStatuses.map(({ statusSeverity }) => statusSeverity)
+    );
+    return aMinSeverity - bMinSeverity;
+  });
 }
