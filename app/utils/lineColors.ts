2405e91eeb6dbcf9b07e2137427ffca0a046e335
icedjello <revues.counter0x@icloud.com>
2024-05-03 20:53:17 +0100

    styling

---
+++ app/utils/lineColors.ts
@@ -0,0 +1,69 @@
+const lineColors: Record<string, Record<string, string>> = {
+  bakerloo: {
+    bg: "bg-amber-800",
+    text: "text-neutral-100",
+    border: "border-neutral-100",
+  },
+  central: {
+    bg: "bg-yellow-400",
+    text: "text-neutral-950",
+    border: "border-neutral-950",
+  },
+  circle: {
+    bg: "bg-red-600",
+    text: "text-neutral-100",
+    border: "border-neutral-100",
+  },
+  district: {
+    bg: "bg-green-600",
+    text: "text-neutral-100",
+    border: "border-neutral-100",
+  },
+  elizabeth: {
+    bg: "bg-purple-600",
+    text: "text-neutral-100",
+    border: "border-neutral-100",
+  },
+  "hammersmith-city": {
+    bg: "bg-pink-400",
+    text: "text-neutral-950",
+    border: "border-neutral-950",
+  },
+  jubilee: {
+    bg: "bg-gray-600",
+    text: "text-neutral-100",
+    border: "border-neutral-100",
+  },
+  "london-overground": {
+    bg: "bg-orange-600",
+    text: "text-neutral-950",
+    border: "border-neutral-950",
+  },
+  metropolitan: {
+    bg: "bg-fuchsia-800",
+    text: "text-neutral-100",
+    border: "border-neutral-100",
+  },
+  northern: {
+    bg: "bg-neutral-800",
+    text: "text-neutral-100",
+    border: "border-neutral-100",
+  },
+  piccadilly: {
+    bg: "bg-indigo-800",
+    text: "text-neutral-100",
+    border: "border-neutral-100",
+  },
+  victoria: {
+    bg: "bg-sky-400",
+    text: "text-neutral-950",
+    border: "border-neutral-950",
+  },
+  "waterloo-city": {
+    bg: "bg-teal-400",
+    text: " text-neutral-950",
+    border: "border-neutral-950",
+  },
+};
+
+export default lineColors;
---
+++ app/utils/severityColors.ts
@@ -0,0 +1,14 @@
+const severityColors = [
+  "bg-red-500 text-neutral-100",
+  "bg-red-500 text-neutral-100",
+  "bg-red-500 text-neutral-100",
+  "bg-red-400 text-neutral-100",
+  "bg-red-400 text-neutral-100",
+  "bg-red-300 text-neutral-800",
+  "bg-red-300 text-neutral-800",
+  "bg-red-200 text-neutral-800",
+  "bg-red-200 text-neutral-800",
+  "bg-green-300 text-neutral-800",
+];
+
+export default severityColors;
